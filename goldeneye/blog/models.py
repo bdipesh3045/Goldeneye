5ec214bd7a31c8dc7fb958253cc8fbb7c863dd4b
Dipesh <bdipesh304@gmail.com>
2024-01-10 10:54:54 +0545

    Remove blog

--- goldeneye/blog/models.py
+++ goldeneye/blog/models.py
@@ -17,7 +17,7 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField("Category", related_name="posts")
-    image = models.ImageField(upload_to="blog", default="")
+    image = models.ImageField(upload_to="blog/", default="")
 
     def __str__(self):
         return self.title
