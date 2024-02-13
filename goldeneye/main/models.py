efb5651ae7f8de12116c307bcd6e8c2da8519467
Surin-p <88964115+Surin-p@users.noreply.github.com>
2024-02-13 16:56:46 +0000

    New Team

--- goldeneye/main/models.py
+++ goldeneye/main/models.py
@@ -41,3 +41,17 @@ class Notification(models.Model):
 
     class Meta:
         ordering = ["-date"]
+
+
+#Team Model
+class TeamModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    post=models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    facebook = models.CharField(max_length=255)
+    image =  models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.id}: {self.first_name} {self.last_name} - {self.post}"
